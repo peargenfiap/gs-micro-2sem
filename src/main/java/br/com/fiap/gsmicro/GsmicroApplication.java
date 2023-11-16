4f37f4484a22c74cc4b29e2bcd8713fcaa1964fa
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-16 19:55:54 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/gsmicro/GsmicroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gsmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/gsmicro/GsmicroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gsmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
