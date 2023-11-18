ea6003e6bc732c39194b1a56d3726f1711461ba2
Pedro <rm88246@fiap.com.br>
2023-11-17 23:27:17 -0300

    style: ajuste lint

--- src/main/java/br/com/fiap/gsmicro/domain/service/impl/ObjetivosServiceImpl.java
+++ src/main/java/br/com/fiap/gsmicro/domain/service/impl/ObjetivosServiceImpl.java
@@ -24,44 +24,44 @@ public class ObjetivosServiceImpl implements ObjetivosServiceUseCase {
 	private IndicatorRestRepository indicatorRepository;
 
 	/**
-	 * Obtém uma lista de Objetivos Gerais.
-	 * Recupera todos os objetos ODS do repositório, 
-	 * converte cada um em um Objetivo Geral e retorna a lista.
+	 * Obtém uma lista de Objetivos Gerais. Recupera todos os objetos ODS do
+	 * repositório, converte cada um em um Objetivo Geral e retorna a lista.
 	 *
 	 * @return Lista de Objetivos Gerais.
 	 */
 	public List<ObjetivoGeral> obterObjetivos() {
-	    return odsRepository.findAll().stream()
-	                         .map(this::buildGeneralObjective)
-	                         .collect(Collectors.toList());
+		return odsRepository
+				.findAll()
+				.stream()
+				.map(this::buildGeneralObjective)
+				.collect(Collectors.toList());
 	}
 
 	/**
-	 * Constrói um Objetivo Geral a partir de um objeto ODS.
-	 * Cria um novo Objetivo Geral, define suas propriedades 
-	 * com base nos dados do ODS e retorna o objetivo montado.
+	 * Constrói um Objetivo Geral a partir de um objeto ODS. Cria um novo Objetivo
+	 * Geral, define suas propriedades com base nos dados do ODS e retorna o
+	 * objetivo montado.
 	 *
 	 * @param ods Objeto ODS do qual o Objetivo Geral será construído.
 	 * @return Objetivo Geral construído a partir do ODS.
 	 */
 	private ObjetivoGeral buildGeneralObjective(Ods ods) {
-	    ObjetivoGeral generalObjective = new ObjetivoGeral();
-	    generalObjective.setId(ods.getOdsKey());
-	    generalObjective.setObjetivos(constructObjective(ods));
-	    generalObjective.setIndicadores(findIndicators(ods.getOdsKey()));
-	    return generalObjective;
+		ObjetivoGeral generalObjective = new ObjetivoGeral();
+		generalObjective.setId(ods.getOdsKey());
+		generalObjective.setObjetivos(constructObjective(ods));
+		generalObjective.setIndicadores(findIndicators(ods.getOdsKey()));
+		return generalObjective;
 	}
 
 	/**
-	 * Constrói um Objetivo a partir de um objeto ODS.
-	 * Cria e retorna um novo Objetivo usando os objetivos 
-	 * brasileiro e global do ODS fornecido.
+	 * Constrói um Objetivo a partir de um objeto ODS. Cria e retorna um novo
+	 * Objetivo usando os objetivos brasileiro e global do ODS fornecido.
 	 *
 	 * @param ods Objeto ODS do qual o Objetivo será construído.
 	 * @return Objetivo criado com base nos objetivos do ODS.
 	 */
 	private Objetivo constructObjective(Ods ods) {
-	    return new Objetivo(ods.getBrazilObjective(), ods.getGlobalObjective());
+		return new Objetivo(ods.getBrazilObjective(), ods.getGlobalObjective());
 	}
 
 	/**
