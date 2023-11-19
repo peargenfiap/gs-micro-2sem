95a2a5223e4252cf3874076eb2a9225532efeb65
peargenfiap <rm88246@fiap.com.br>
2023-11-19 09:29:43 -0300

    fix: endpoint alterado para receber o indicatorKey via queryParam e não via path

--- src/main/java/br/com/fiap/gsmicro/controller/impl/ConsumeController.java
+++ src/main/java/br/com/fiap/gsmicro/controller/impl/ConsumeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.gsmicro.controller.usecase.ConsumeUseCase;
@@ -26,8 +28,8 @@ public class ConsumeController implements ConsumeUseCase {
 		return ResponseEntity.ok(consumes);
 	}
 
-	@GetMapping("/porindicador/{indicatorKey}")
-	public ResponseEntity<List<Consume>> obterListaConsumePorIdIndicator(@PathVariable String indicatorKey) {
+	@GetMapping("/porindicador")
+	public ResponseEntity<List<Consume>> obterListaConsumePorIdIndicator(@RequestParam(name = "indicatorKey", required = true) String indicatorKey) {
 		var consumes = consumeRepository.findByIndicatorKey(indicatorKey);
 		return ResponseEntity.ok(consumes);
 	}
